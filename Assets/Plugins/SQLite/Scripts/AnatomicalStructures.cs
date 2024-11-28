f614010ead2fe3e692123533a64d1f91a9d565b2
Mathias <hamedingermathias@gmail.com>
2024-11-28 15:54:30 +0100

    small changes

--- Assets/Plugins/SQLite/Scripts/AnatomicalStructures.cs
+++ Assets/Plugins/SQLite/Scripts/AnatomicalStructures.cs
@@ -7,9 +7,10 @@ public class AnatomicalStructures  {
 	public string german_name { get; set; }
 	public string latin_name { get; set; }
 	public string category { get; set; }
+	public int parentId {get; set;}
 
 	public override string ToString ()
 	{
-		return string.Format ("[AnatomicalStructures: id={0}, german_name={1},  latin_name={2}, category{3}]", id, german_name, latin_name, category);
+		return string.Format ("[AnatomicalStructures: id={0}, german_name={1},  latin_name={2}, category={3}, parentId={4}]", id, german_name, latin_name, category, parentId);
 	}
 }
--- Assets/Plugins/SQLite/Scripts/DataService.cs
+++ Assets/Plugins/SQLite/Scripts/DataService.cs
@@ -15,41 +15,37 @@ public class DataService  {
 #if UNITY_EDITOR
             var dbPath = string.Format(@"Assets/StreamingAssets/{0}", DatabaseName);
 #else
-        // check if file exists in Application.persistentDataPath
+       
         var filepath = string.Format("{0}/{1}", Application.persistentDataPath, DatabaseName);
 
         if (!File.Exists(filepath))
         {
             Debug.Log("Database not in Persistent path");
-            // if it doesn't ->
-            // open StreamingAssets directory and load the db ->
 
 #if UNITY_ANDROID 
-            var loadDb = new WWW("jar:file://" + Application.dataPath + "!/assets/" + DatabaseName);  // this is the path to your StreamingAssets in android
-            while (!loadDb.isDone) { }  // CAREFUL here, for safety reasons you shouldn't let this while loop unattended, place a timer and error check
-            // then save to Application.persistentDataPath
+            var loadDb = new WWW("jar:file://" + Application.dataPath + "!/assets/" + DatabaseName);
+            while (!loadDb.isDone) { } 
+            
             File.WriteAllBytes(filepath, loadDb.bytes);
 #elif UNITY_IOS
-                 var loadDb = Application.dataPath + "/Raw/" + DatabaseName;  // this is the path to your StreamingAssets in iOS
-                // then save to Application.persistentDataPath
+                 var loadDb = Application.dataPath + "/Raw/" + DatabaseName;
+                
                 File.Copy(loadDb, filepath);
 #elif UNITY_WP8
-                var loadDb = Application.dataPath + "/StreamingAssets/" + DatabaseName;  // this is the path to your StreamingAssets in iOS
-                // then save to Application.persistentDataPath
+                var loadDb = Application.dataPath + "/StreamingAssets/" + DatabaseName;
+               
                 File.Copy(loadDb, filepath);
 
 #elif UNITY_WINRT
-		var loadDb = Application.dataPath + "/StreamingAssets/" + DatabaseName;  // this is the path to your StreamingAssets in iOS
-		// then save to Application.persistentDataPath
+		var loadDb = Application.dataPath + "/StreamingAssets/" + DatabaseName;
 		File.Copy(loadDb, filepath);
 		
 #elif UNITY_STANDALONE_OSX
-		var loadDb = Application.dataPath + "/Resources/Data/StreamingAssets/" + DatabaseName;  // this is the path to your StreamingAssets in iOS
-		// then save to Application.persistentDataPath
+		var loadDb = Application.dataPath + "/Resources/Data/StreamingAssets/" + DatabaseName;
+		
 		File.Copy(loadDb, filepath);
 #else
-	var loadDb = Application.dataPath + "/StreamingAssets/" + DatabaseName;  // this is the path to your StreamingAssets in iOS
-	// then save to Application.persistentDataPath
+	var loadDb = Application.dataPath + "/StreamingAssets/" + DatabaseName;
 	File.Copy(loadDb, filepath);
 
 #endif
@@ -78,7 +74,8 @@ public class DataService  {
     		id = 1,
     		german_name = "Eckzahn",
     		latin_name = "latinEckzahn",
-    		category = "Mund"
+    		category = "Mund",
+			parentId = 3	
 		});
 
 		_connection.Insert(new Descriptions{
